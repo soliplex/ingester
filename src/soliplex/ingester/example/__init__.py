323d0518f2b486f6aca57d3e4ba2de79640482d6
bryan davis <bryan@monkeytronics.org>
2025-12-22 10:21:10 -0600

    more pagination

--- src/soliplex/ingester/example/__init__.py
+++ src/soliplex/ingester/example/__init__.py
@@ -38,6 +38,10 @@ async def parse_document(batch_id: int = None, doc_hash: str = None, source: str
     logger.info(f"parse_document started  source={source} batch_id={batch_id} doc_hash={doc_hash}")
 
 
+async def embed_document(batch_id: int = None, doc_hash: str = None, source: str = None):
+    logger.info(f"embed_document started  source={source} batch_id={batch_id} doc_hash={doc_hash}")
+
+
 async def save_document(
     batch_id: int = None,
     doc_hash: str = None,
